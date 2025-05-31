7a98ab510ef43ca43e72483442f25688d23c1a2e
Theproinfo <theitproinfo@gmail.com>
2025-05-30 20:40:40 -0400

    day14-2

---
+++ day14-2/ASP.netCore.MVCCRUD.day14.demo1/ASP.netCore.MVCCRUD.day14.demo1/Controllers/StudentController.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel;
+using ASP.netCore.MVCCRUD.day14.demo1.Models;
+using ASP.netCore.MVCCRUD.day14.demo1.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace ASP.netCore.MVCCRUD.day14.demo1.Controllers;
+public class StudentController : Controller
+{
+   
+    public IActionResult Index()
+    {
+        string myDbConnectionString = AppConfigurationService._configuration.GetConnectionString("myConnectionString");
+
+        ViewBag.connStr = myDbConnectionString;
+
+        // for testing insert one record -executeNonquery
+        // inset update delete
+        // hard code for testing
+
+        #region MyRegion - not used sql helper
+
+        //        using (SqlConnection conn=new SqlConnection(myDbConnectionString))
+        //        {
+        //            conn.Open();
+        //            string sqlTxt = @"
+        //                insert into Students (FirstName,LastName,Email)
+        //values('Alice100','Wang100','abc@hotmail.com');
+        //                    ";
+
+        //            using (SqlCommand command=new SqlCommand(sqlTxt,conn))
+        //            {
+        //                int rows= command.ExecuteNonQuery();
+
+        //                if (rows > 0)
+        //                {
+        //                    ViewBag.mymess = "insert successfully";
+        //                }
+        //                else
+        //                {
+        //                    ViewBag.mymess = "insert failure";
+        //                }
+        //            }
+
+
+        //        }
+
+        #endregion
+
+        string sqlTxt = @"insert into Students (FirstName,LastName,Email)
+values('Alice200','Wang200','abc200@hotmail.com');";
+
+        int rows = MSSQLHelper.ExecuteNoQuery(sqlTxt);
+
+        if (rows > 0)
+        {
+            ViewBag.mymess = "inset one record";
+        }
+        else
+        {
+            ViewBag.mymess = "insert failure";
+        }
+
+        return View();
+    }
+
+    public IActionResult Details()
+    {
+       //
+       string sqlTxt = @"
+          SELECT COUNT(*) FROM Students";
+
+       object totals = MSSQLHelper.GetScalar(sqlTxt);
+       int totalStudents = Convert.ToInt32(totals);
+       ViewBag.mess = totalStudents;
+        return View();
+    }
+}
---
+++ day14-2/ASP.netCore.MVCCRUD.day14.demo1/ASP.netCore.MVCCRUD.day14.demo1/Models/MSSQLHelper.cs
@@ -0,0 +1,70 @@
+﻿using System.Data;
+using ASP.netCore.MVCCRUD.day14.demo1.Services;
+using Microsoft.Data.SqlClient;
+
+namespace ASP.netCore.MVCCRUD.day14.demo1.Models;
+
+public static class MSSQLHelper
+{
+    public static readonly string connStr =
+        AppConfigurationService._configuration.GetConnectionString("myConnectionString");
+
+    /// <summary>
+    /// pass one sql script, execute noQuery (inset/delete/update)
+    /// </summary>
+    /// <param name="sqlTxt"></param>
+    /// <returns>int affected rows</returns>
+    public static int ExecuteNoQuery(string sqlTxt)
+    {
+        using (SqlConnection SqlConn=new SqlConnection(connStr))
+        {
+            SqlConn.Open(); // open dataBase
+            SqlCommand sqlCommand=SqlConn.CreateCommand();
+            sqlCommand.CommandText=sqlTxt;
+            sqlCommand.CommandType = CommandType.Text; // not stored procedure
+
+            return sqlCommand.ExecuteNonQuery();
+
+
+
+        }
+
+    } //end of executeNoquery
+
+    /// <summary>
+    /// get record from the table
+    /// </summary>
+    /// <param name="sqlTxt"></param>
+    /// <returns></returns>
+    public static SqlDataReader GetDataReader(string sqlTxt)
+    {
+        SqlConnection sqlConn = new SqlConnection(connStr);
+        sqlConn.Open(); // open database
+        SqlCommand sqlCommand=sqlConn.CreateCommand();
+        sqlCommand.CommandText=sqlTxt;
+        sqlCommand.CommandType=CommandType.Text; // execute sql script, not stored procedure
+        // when closed the database, will automaticall close teh connection
+        return sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+
+    }
+
+    /// <summary>
+    /// get value of the first colummn first row
+    /// </summary>
+    /// <param name="sqlTxt"></param>
+    /// <returns></returns>
+    public static object GetScalar(string sqlTxt)
+    {
+        using (SqlConnection sqlConn=new SqlConnection(connStr))
+        {
+            sqlConn.Open(); // open database
+            SqlCommand sqlCommand = sqlConn.CreateCommand();
+            sqlCommand.CommandText=sqlTxt;
+            sqlCommand.CommandType= CommandType.Text;
+            return sqlCommand.ExecuteScalar();
+
+        }
+    }
+
+
+} //end of class
