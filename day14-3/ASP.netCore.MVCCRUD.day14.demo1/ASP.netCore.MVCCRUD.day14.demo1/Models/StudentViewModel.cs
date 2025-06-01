088618b951b6e55b0d78802d23c957991cf1195a
Theproinfo <theitproinfo@gmail.com>
2025-06-01 13:31:49 -0400

    day project

---
+++ day14-3/ASP.netCore.MVCCRUD.day14.demo1/ASP.netCore.MVCCRUD.day14.demo1/Models/StudentViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.netCore.MVCCRUD.day14.demo1.Models;
+
+public class StudentViewModel
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; }
+}
---
+++ day14-3/ASP.netCore.MVCCRUD.day14.demo1/ASP.netCore.MVCCRUD.day14.demo1/Services/AppConfigurationService.cs
@@ -0,0 +1,27 @@
+﻿
+namespace ASP.netCore.MVCCRUD.day14.demo1.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+public class AppConfigurationService
+{
+    
+    
+    public static IConfiguration _configuration { get; set; }
+    
+    //static method
+    static AppConfigurationService()
+    {
+        //_configuration = new ConfigurationBuilder()
+        //    .Add(new JsonConfigurationSource( {Path = "appsettings.json", ReloadOnChange = true}).Build();
+
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory()) // sets base path for appsettings.json
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+    }
+
+
+
+
+}
